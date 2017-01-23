bae531b3d38e35320254d64eedb80b197499e4af
root <root@localhost>
2017-01-23 07:48:11 +0000

    Initial commit

---
+++ app/src/main/java/nodomain/basicsquirrel/batterymanager/MainActivity.java
@@ -0,0 +1,14 @@
+package nodomain.basicsquirrel.batterymanager;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
